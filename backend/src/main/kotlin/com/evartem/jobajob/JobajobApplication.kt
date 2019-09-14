c9a58049e64ee3ea79d566f3e9ac02e99fdcb9b5
Evgeniy Plokhov <plokhov@gmail.com>
2019-09-14 22:16:19 +0500

    Initial commit

---
+++ backend/src/main/kotlin/com/evartem/jobajob/JobajobApplication.kt
@@ -0,0 +1,11 @@
+package com.evartem.jobajob
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JobajobApplication
+
+fun main(args: Array<String>) {
+	runApplication<JobajobApplication>(*args)
+}

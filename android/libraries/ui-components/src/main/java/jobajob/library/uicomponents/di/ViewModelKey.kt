9c87988b3f97564a7bfacc5bd46163078a3af810
Evgeniy Plokhov <plokhov@gmail.com>
2019-10-01 12:20:15 +0500

    Add "ui-component" library

---
+++ android/libraries/ui-components/src/main/java/jobajob/library/uicomponents/di/ViewModelKey.kt
@@ -0,0 +1,9 @@
+package jobajob.library.uicomponents.di
+
+import androidx.lifecycle.ViewModel
+import dagger.MapKey
+import kotlin.reflect.KClass
+
+@Retention(AnnotationRetention.RUNTIME)
+@MapKey
+annotation class ViewModelKey(val value: KClass<out ViewModel>)

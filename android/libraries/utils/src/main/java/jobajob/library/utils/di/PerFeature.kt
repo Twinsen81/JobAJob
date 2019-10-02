c744c3f1add61197bf9136e98f7bf4d80d8153b0
Evgeniy Plokhov <plokhov@gmail.com>
2019-10-02 12:16:36 +0500

    Add comments

--- android/libraries/utils/src/main/java/jobajob/library/utils/di/PerFeature.kt
+++ android/libraries/utils/src/main/java/jobajob/library/utils/di/PerFeature.kt
@@ -2,6 +2,9 @@ package jobajob.library.utils.di
 
 import javax.inject.Scope
 
+/**
+ * A Dagger's scope for an object that lives when a feature is active.
+ */
 @Scope
 @Retention(AnnotationRetention.RUNTIME)
 annotation class PerFeature

94ff9984b341570dab16753842010ba693317400
Evgeniy Plokhov <plokhov@gmail.com>
2019-10-08 11:40:40 +0500

    Add base classes for interactors

---
+++ android/libraries/model/src/main/java/jobajob/library/intercator/ObservableUseCase.kt
@@ -0,0 +1,20 @@
+package jobajob.library.intercator
+
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.schedulers.Schedulers
+
+abstract class ObservableUseCase<T, in Params> constructor(
+    private val observeOn: Scheduler
+) : UseCase() {
+
+    protected abstract fun buildUseCaseObservable(params: Params? = null): Observable<T>
+
+    open operator fun invoke(singleObserver: DisposableObserver<T>, params: Params? = null) {
+        val single = this.buildUseCaseObservable(params)
+            .subscribeOn(Schedulers.io())
+            .observeOn(observeOn)
+        addDisposable(single.subscribeWith(singleObserver))
+    }
+}
---
+++ android/libraries/model/src/main/java/jobajob/library/intercator/UseCase.kt
@@ -0,0 +1,27 @@
+package jobajob.library.intercator
+
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+abstract class UseCase {
+
+    private var lastDisposable: Disposable? = null
+    private val compositeDisposable = CompositeDisposable()
+
+    protected fun addDisposable(d: Disposable) {
+        compositeDisposable.add(d)
+        lastDisposable = d
+    }
+
+    fun disposeLast() {
+        lastDisposable?.let {
+            if (!it.isDisposed) {
+                it.dispose()
+            }
+        }
+    }
+
+    fun dispose() {
+        compositeDisposable.clear()
+    }
+}

f2a7dc5be0e4594caeea286858adc550d2c3ce0c
Evgeniy Plokhov <plokhov@gmail.com>
2019-10-09 00:21:03 +0500

    Add base viewmodel, implement vacancies viewmodel

--- android/features/dashboard/dashboard-impl/src/main/java/jobajob/feature/dashboard/presentation/vacancies/VacanciesViewModel.kt
+++ android/features/dashboard/dashboard-impl/src/main/java/jobajob/feature/dashboard/presentation/vacancies/VacanciesViewModel.kt
@@ -1,6 +1,26 @@
 package jobajob.feature.dashboard.presentation.vacancies
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import jobajob.feature.dashboard.domain.intercator.GetVacanciesUseCase
+import jobajob.library.entity.vacancy.Vacancy
+import jobajob.library.uicomponents.presentation.BaseViewModel
 import javax.inject.Inject
+import jobajob.library.entity.common.Result
 
-internal class VacanciesViewModel @Inject constructor(): ViewModel()
+internal class VacanciesViewModel @Inject constructor(
+    private val getVacanciesUseCase: GetVacanciesUseCase
+) : BaseViewModel(getVacanciesUseCase) {
+
+    private val _vacancies: MutableLiveData<List<Vacancy>> = MutableLiveData()
+    val vacancies get() = _vacancies as LiveData<List<Vacancy>>
+
+    fun loadVacancies() {
+        getVacanciesUseCase(singleObserver { vacanciesListResult ->
+            when (vacanciesListResult) {
+                is Result.Success -> _vacancies.value = vacanciesListResult.value
+                is Result.Error -> handleFailure(vacanciesListResult.error)
+            }
+        })
+    }
+}
---
+++ android/libraries/ui-components/src/main/java/jobajob/library/uicomponents/presentation/BaseViewModel.kt
@@ -0,0 +1,58 @@
+package jobajob.library.uicomponents.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import io.reactivex.observers.DisposableObserver
+import io.reactivex.observers.DisposableSingleObserver
+import jobajob.library.entity.common.Failure
+import jobajob.library.intercator.UseCase
+
+/**
+ * Base class for view models that provides automatic observer disposing,
+ * failure handling and observer creation convenience functions.
+ */
+open class BaseViewModel(vararg useCases: UseCase) : ViewModel() {
+
+    private val disposableUseCases = useCases
+
+    private val _failure = SingleLiveEvent<Failure>()
+    val failure: LiveData<Failure>
+        get() = _failure
+
+    protected fun handleFailure(failure: Failure) {
+        _failure.value = failure
+    }
+
+    protected fun <T> observer(onNext: (T) -> Unit, onComplete: (() -> Unit)? = null) =
+        object : DisposableObserver<T>() {
+
+            override fun onComplete() {
+                onComplete?.invoke()
+            }
+
+            override fun onNext(t: T) {
+                onNext.invoke(t)
+            }
+
+            override fun onError(e: Throwable) {
+                handleFailure(Failure.ApplicationError(e))
+            }
+        }
+
+    protected fun <T> singleObserver(onSuccess: (T) -> Unit) =
+        object : DisposableSingleObserver<T>() {
+
+            override fun onSuccess(t: T) {
+                onSuccess.invoke(t)
+            }
+
+            override fun onError(e: Throwable) {
+                handleFailure(Failure.ApplicationError(e))
+            }
+        }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposableUseCases.forEach { it.dispose() }
+    }
+}

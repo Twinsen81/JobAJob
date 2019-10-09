4d2a8fedfa1feb2fd0694a91cd818d5f1c79ac8f
Evgeniy Plokhov <plokhov@gmail.com>
2019-10-10 00:21:20 +0500

    Add the "company" filed to the Vacancy entity

--- android/libraries/core/src/main/java/jobajob/library/entity/vacancy/Vacancy.kt
+++ android/libraries/core/src/main/java/jobajob/library/entity/vacancy/Vacancy.kt
@@ -3,6 +3,7 @@ package jobajob.library.entity.vacancy
 data class Vacancy(
     val id: Long = 0,
     val title: String = "",
+    val company: String = "",
     val details: String = "",
     val salary: String = "",
     val payed: Boolean = false

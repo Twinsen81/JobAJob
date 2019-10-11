2395e0015b16c57372bb5173a981e1b3d1e6b347
Evgeniy Plokhov <plokhov@gmail.com>
2019-10-11 13:30:31 +0500

    Remove default values from Vacancy model

--- android/libraries/core/src/main/java/jobajob/library/entity/vacancy/Vacancy.kt
+++ android/libraries/core/src/main/java/jobajob/library/entity/vacancy/Vacancy.kt
@@ -1,10 +1,10 @@
 package jobajob.library.entity.vacancy
 
 data class Vacancy(
-    val id: Long = 0,
-    val title: String = "",
-    val company: String = "",
-    val details: String = "",
-    val salary: String = "",
-    val payed: Boolean = false
+    val id: Long,
+    val title: String,
+    val company: String,
+    val details: String,
+    val salary: String,
+    val payed: Boolean
 )
